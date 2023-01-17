30e56a5733cc5be170ec1dda6841dea0a5e86701
AbelAshine1234 <abelashineszreal@gmail.com>
2023-01-17 22:31:14 +0300

    Easy counting

---
+++ Hackerrank/counting.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    
+    max_value = max(arr)+1
+    if max_value < 100:
+        max_value += 1
+    count_arr = [0]*max_value
+    for elt in arr:
+        count_arr[elt] += 1 
+    return count_arr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
